a331ae67f39cec28ce2af87a7849e153ddb149c5
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-09-22 16:34:37 +0530

    commit

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product
+
 
 # Create your views here.
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-  
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
