0222382378535e90fe2cae831b212966ac1d01ca
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-09-26 11:47:44 +0530

    delete operation

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -19,9 +19,35 @@ def product_list(request):
         serializer = ProductSerializer(data=request.data)
         # serializer.is_valid(raise_exception=True)
         if serializer.is_valid():
+            serializer.save()
             return Response(request.data, status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "GET":
+        serializer = ProductSerializer(product, context={"request": request})
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(
+            product, data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        if product.orderitem_set.count() > 0:
+            return Response(
+                {
+                    "error": "Product cannot be deleted because it is associated with an order item."
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
